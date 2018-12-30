7035a5ba4444a484574550544cf73bc53c989545
supercym <cym467000@163.com>
2018-12-30 16:51:34 +0800

    scheduler & streamserver

---
+++ scheduler/dbops/api.go
@@ -0,0 +1,16 @@
+package dbops
+
+func AddVideoDeletionRecord(vid string) error {
+	stmtIns, err := dbConn.Prepare("INSERT INTO video_del_rec (video_id) VALUES(?)")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmtIns.Exec(vid)
+	if err != nil {
+		return err
+	}
+
+	defer stmtIns.Close()
+	return nil
+}
