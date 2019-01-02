a7d4160220d226e272e30227a7f03fdd4648d2df
supercym <cym467000@163.com>
2019-01-02 19:20:55 +0800

    initial UI

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@
 
 cd /root/golang/gopath/src/StreamingWebsite/web
 go install
-cp /root/golang/goapth/bin/web /root/golang/goapth/bin/video_server_web_ui/web
-cp -R /root/golang/gopath/src/StreamingWebsite/templates /root/golang/goapth/bin/video_server_web_ui/
+cp /root/golang/gopath/bin/web /root/golang/goapth/bin/video_server_web_ui/web
+cp -R /root/golang/gopath/src/StreamingWebsite/templates /root/golang/gopath/bin/video_server_web_ui/
 
